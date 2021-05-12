e3956b64be0a580bc40b9cfd32364a5cd616bf1b
Tamas Szasz <tqmi.szasz@gmail.com>
2021-05-12 14:19:51 +0300

    empty service, eureka client enabled

---
+++ server/services/storage_service/src/test/java/com/dpdm/storage_service/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpdm.storage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
