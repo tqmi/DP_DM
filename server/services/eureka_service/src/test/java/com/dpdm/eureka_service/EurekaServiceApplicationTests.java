fbfb8525fca65eef7f8bf50fb001e06e27efb3f6
Tamas Szasz <tqmi.szasz@gmail.com>
2021-05-12 12:59:45 +0300

    simple eureka server service

---
+++ server/services/eureka_service/src/test/java/com/dpdm/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpdm.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
