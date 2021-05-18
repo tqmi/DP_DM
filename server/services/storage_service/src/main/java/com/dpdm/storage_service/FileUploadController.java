585828220ef92a908b3e55da303b192766e6335b
Tamas Szasz <tqmi.szasz@gmail.com>
2021-05-18 11:41:21 +0300

    organized imports

--- server/services/storage_service/src/main/java/com/dpdm/storage_service/FileUploadController.java
+++ server/services/storage_service/src/main/java/com/dpdm/storage_service/FileUploadController.java
@@ -1,9 +1,7 @@
 package com.dpdm.storage_service;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -12,27 +10,16 @@ import com.dpdm.storageapi.controllers.FilesApiController;
 import com.dpdm.storageapi.model.FileResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @CrossOrigin
 @RestController
