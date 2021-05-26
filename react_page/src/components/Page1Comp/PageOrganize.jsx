a091e96acbf9a66c5b40f93468effed6b94817a9
Petronela Vieru <petronela.vieru@student.upt.ro>
2021-05-26 23:29:19 +0300

    Page 1 organize

--- react_page/src/components/Page1Comp/PageOrganize.jsx
+++ react_page/src/components/Page1Comp/PageOrganize.jsx
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 import "./PageOrganize.css";
+import SearchField from "react-search-field";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { useOperationMethod } from "react-openapi-client";
+import sendReqWithToken from "../SendReqWithToken";
+
+import { auth, firebase } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { useHistory } from "react-router-dom";
 
 
-const PER_PAGE = 9;
+const PER_PAGE = 4;
+
+var institutions=[];
+
 
 export default function PageOrganize() {
   const [currentPage, setCurrentPage] = useState(0);
   const [data, setData] = useState([]);
+  const [getInstitutions, { loading, info, error }] =
+    useOperationMethod("getInstitutions");
+
+    
+  const [user] = useAuthState(auth);
 
   useEffect(() => {
-    fetchData();
+    sendReqWithToken(
+      user,
+      getInstitutions,
+      null,
+      null,
+      {},
+      getInstitutionStatus
+    );
   }, []);
-
-  function fetchData() {
-    fetch("https://ihsavru.me/Demo/uploads.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const {
-          course: { uploads },
-        } = data;
-        setData(uploads);
-      });
+  function getInstitutionStatus(resp) {
+    console.log(resp);
+    setData(resp.data);
   }
 
   function handlePageClick({ selected: selectedPage }) {
@@ -31,31 +48,49 @@ export default function PageOrganize() {
   const offset = currentPage * PER_PAGE;
   const displayNumber = 0;
 
-    
-    
-    const currentPageData = <ul class="flex-container wrap">{data .slice(offset, offset + PER_PAGE).map(({uploader,id}) => (<button type="button" class="btn btn-light" >{uploader} {id}</button>
-    ))}</ul>
-    
- 
+  const currentPageData = (
+    <ul class="flex-container wrap">
+      {data.slice(offset, offset + PER_PAGE).map(({ name }) => (
+        <button type="button" class="btn btn-light" >
+          {name}
+          
+        </button>
+      ))}
+    </ul>
+  );
 
   const pageCount = Math.ceil(data.length / PER_PAGE);
 
   return (
     <div className="PageOrganize">
       <h1>Institutions</h1>
-      <ReactPaginate
-        previousLabel={"← Previous"}
-        nextLabel={"Next →"}
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        previousLinkClassName={"pagination__link"}
-        nextLinkClassName={"pagination__link"}
-        disabledClassName={"pagination__link--disabled"}
-        activeClassName={"pagination__link--active"}
-      />
+      <div class="container">
+        <ul class="flex-container nowrap">
+          <li class="flex-item1">
+            <ReactPaginate
+              previousLabel={"← Previous"}
+              nextLabel={"Next →"}
+              pageCount={pageCount}
+              onPageChange={handlePageClick}
+              containerClassName={"pagination"}
+              previousLinkClassName={"pagination__link"}
+              nextLinkClassName={"pagination__link"}
+              disabledClassName={"pagination__link--disabled"}
+              activeClassName={"pagination__link--active"}
+            />
+          </li>
+          <li class="flex-item2">
+            <SearchField
+              placeholder="Search..."
+              //onChange={onChange}
+              //searchText="Search"
+              classNames="test-class"
+            />
+          </li>
+        </ul>
 
-      {currentPageData}
+        {currentPageData}
+      </div>
     </div>
   );
 }
